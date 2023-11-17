97b54fb731dd6aa711a9e4808020316b840fc2b5
bahossdev <148646212+bahossdev@users.noreply.github.com>
2023-11-16 23:22:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,151 +1,98 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// // Function declaration
-// function generatePassword() {
-
-// }
-// it has to be prompted with click on the bottun NOT on page opening!!
-// 1st prompt: Welcome to Password Generator! Please press Okay to continue.
-
-
+// assign variables for lowercase, uppercase, number, and special characters
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var number = '0123456789';
 var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"';
-var typeN = 0;
-var lowerCaseN = 1;
-var upperCaseN = 1;
-var numberN = 1;
-var specialCharN =1;
-var passLength = window.prompt("Choose a length between 8 and 128.");
 
 
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", function(){
 
+  // used in calculating the iterations of the for loop
+  var typeN = 0;
 
-while (passLength < 8 || passLength > 128) {
-  passLength = prompt("You must choose a length between 8 and 128!");
-};
+  // open prompt to get the password length from the user
+  var passLength = window.prompt("Choose a length between 8 and 128.");
 
-
-var lowerCaseSelect = window.confirm("Do you want your password to have any 🔡 lowercase letters?");
-console.log(lowerCaseSelect);
-if (lowerCaseSelect) {
-  lowerCaseN = 0;
-};
-
-var upperCaseSelect = window.confirm("Do you want your password to have any 🔠 uppercase letters?");
-console.log(upperCaseSelect);
-if (upperCaseSelect) {
-  upperCaseN = 0;
-};
-
-var numberSelect = window.confirm("Do you want your password to have any 🔢 numbers?");
-console.log(numberSelect);
-if (numberSelect) {
-  numberN = 0;
-};
-
-var specialSelect = window.confirm("Do you want your password to have any 🔣 special characters?");
-console.log(specialSelect);
-if (specialSelect) {
-  specialCharN = 0;
-};
-
-while (!(lowerCaseSelect || upperCaseSelect || numberSelect || specialSelect)){
-  alert("You must choose at lease one criteria!⛔️");
-  var lowerCaseSelect = window.confirm("Do you want your password to have any 🔡 lowercase letters?");
-  console.log(lowerCaseSelect);
-  if (lowerCaseSelect) {
-    lowerCaseN = 0;
+  // prevent from input smaller than 8 or greater than 128 
+  while (passLength < 8 || passLength > 128) {
+    passLength = prompt("You must choose a length between 8 and 128!");
   };
+
+  // several prompts for selecting the criteria
+  var lowerCaseSelect = window.confirm("Do you want your password to have any 🔡 lowercase letters?");
 
   var upperCaseSelect = window.confirm("Do you want your password to have any 🔠 uppercase letters?");
-  console.log(upperCaseSelect);
-  if (upperCaseSelect) {
-    upperCaseN = 0;
-  };
 
   var numberSelect = window.confirm("Do you want your password to have any 🔢 numbers?");
-  console.log(numberSelect);
-  if (numberSelect) {
-    numberN = 0;
-  };
 
   var specialSelect = window.confirm("Do you want your password to have any 🔣 special characters?");
-  console.log(specialSelect);
-  if (specialSelect) {
-    specialCharN = 0;
+
+  // validate that at least one criteria is selected
+  while (!(lowerCaseSelect || upperCaseSelect || numberSelect || specialSelect)){
+    alert("You must choose at lease one criteria!⛔️");
+    var lowerCaseSelect = window.confirm("Do you want your password to have any 🔡 lowercase letters?");
+
+    var upperCaseSelect = window.confirm("Do you want your password to have any 🔠 uppercase letters?");
+
+    var numberSelect = window.confirm("Do you want your password to have any 🔢 numbers?");
+
+    var specialSelect = window.confirm("Do you want your password to have any 🔣 special characters?");
   };
-};
-if (lowerCaseSelect){
-  typeN++;
-};
-if (upperCaseSelect){
-  typeN++;
-};
-if (numberSelect){
-  typeN++;
-};
-if (specialSelect){
-  typeN++;
-};
-console.log(typeN);
 
-// if (!(lowerCaseSelect || upperCaseSelect || numberSelect || specialSelect)){
-//   alert("You must choose at lease one criteria!⛔️");
-//   askUser();
-// }
+  // count the number of selected types(criteria)
+  if (lowerCaseSelect){
+    typeN++;
+  };
+  if (upperCaseSelect){
+    typeN++;
+  };
+  if (numberSelect){
+    typeN++;
+  };
+  if (specialSelect){
+    typeN++;
+  };
 
-
-
-
-function generatePassword(length) {
-  let result = '';
-  for ( let i = 0; i < length; i++ ) {
-    if (lowerCaseSelect){
-      var lowerCaseLength = lowerCase.length;
-      result += lowerCase.charAt(Math.floor(Math.random() * lowerCaseLength));
-      lowerCaseN++;
+// function declaration: generate password based on the selected criteria
+  function generatePassword(length) {
+    let result = '';
+      for ( let i = 0; i < length; i++ ) {
+        if (lowerCaseSelect){
+          var lowerCaseLength = lowerCase.length;
+          result += lowerCase.charAt(Math.floor(Math.random() * lowerCaseLength));
+        };
+        if (upperCaseSelect){
+          var upperCaseLength = upperCase.length;
+          result += upperCase.charAt(Math.floor(Math.random() * upperCaseLength));
+        };
+        if (numberSelect){
+          var numberLength = number.length;
+          result += number.charAt(Math.floor(Math.random() * numberLength));
+        };
+        if (specialSelect){
+          var specialCharLength = specialChar.length;
+          result += specialChar.charAt(Math.floor(Math.random() * specialCharLength));
+        };
+      };
+      return result;
     };
-    if (upperCaseSelect){
-      var upperCaseLength = upperCase.length;
-      result += upperCase.charAt(Math.floor(Math.random() * upperCaseLength));
-      upperCaseN++;
-    };
-    if (numberSelect){
-      var numberLength = number.length;
-      result += number.charAt(Math.floor(Math.random() * numberLength));
-      numberN++;
-    };
-    if (specialSelect){
-      var specialCharLength = specialChar.length;
-      result += specialChar.charAt(Math.floor(Math.random() * specialCharLength));
-      specialCharN++;
-    };
-    console.log(result);
-};
-return result;
-};
-var iteration = Math.floor(passLength/typeN) + 1;
+  
+    // calculate the iteration needed
+    var iteration = Math.floor(passLength/typeN) + 1;
 
-var longPassword = generatePassword(iteration);
-var l = longPassword.length;
-var p = Number(passLength);
-var n = l - p;
-console.log(l, p, n);
-var password = longPassword.slice(n);
-console.log(password);
+    // create the long password
+    var longPassword = generatePassword(iteration);
+
+    // adjust the length
+    var password = longPassword.slice((longPassword.length)-Number(passLength));
+
+    // write password to the #password input
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  );
